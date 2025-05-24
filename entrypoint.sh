4d8973e95746280b770e0b1364c4ffdb593da174
Yilmaz Mustafa <ymus@tuta.io>
2025-05-24 14:30:49 +0200

    Remove Dockerfile, entrypoint script, and run-dock script as part of project cleanup, eliminating unnecessary containerization components.

--- entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pulseaudio --start --exit-idle-time=-1
-/app/dist/instyper
--- run-dock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run --rm -it \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v ~/.Xauthority:/home/appuser/.Xauthority \
-  -e DISPLAY \
-  --device /dev/snd \
-  instyper
